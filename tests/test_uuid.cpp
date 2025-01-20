fc555d0af112c668efe007800d92b3a3aa351835
Douglas Ronne <vextorspace@gmail.com>
2025-01-20 13:48:57 -0400

    random uuids

--- tests/test_uuid.cpp
+++ tests/test_uuid.cpp
@@ -8,3 +8,9 @@ TEST(UUID, is_string_with_atleast_36) {
     string id = UuidGenerator().generate();
     ASSERT_GE(id.size(), 36);
 }
+
+TEST(UUID, no_same_twice) {
+    string id1 = UuidGenerator().generate();
+    string id2 = UuidGenerator().generate();
+    ASSERT_NE(id1, id2);
+}
