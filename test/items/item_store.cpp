cbf8976f76f92f1f756bc50d1075adc425c8ffcc
Douglas Ronne <vextorspace@gmail.com>
2025-01-24 09:54:39 -0500

    added guard tests

--- test/items/item_store.cpp
+++ test/items/item_store.cpp
@@ -8,3 +8,15 @@ TEST(ItemStore, constructs_with_new_root_item) {
     const Item * root = store.get_root();
     ASSERT_EQ(root->get_content(), "root");
 }
+
+TEST(ItemStore, get_root_returns_same_item) {
+    ItemStore store = ItemStore();
+    const Item * root = store.get_root();
+    ASSERT_EQ(root, store.get_root());
+}
+
+TEST(ItemStore, different_store_gives_different_root) {
+    ItemStore store1 = ItemStore();
+    ItemStore store2 = ItemStore();
+    ASSERT_NE(store1.get_root(), store2.get_root());
+}
